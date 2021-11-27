aaa4c28e6b7cc68de07ca5016e745e89a2c3145a
PengShaw <hhxp9106@gmail.com>
2021-11-27 23:40:19 +0800

    package reading config struct by viper in config.go

---
+++ cmd-config/config.go
@@ -0,0 +1,24 @@
+package cmdconfig
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+func SetConfigFlag(cmd *cobra.Command) {
+	cmd.PersistentFlags().StringVar(&cfgFile, "config", "./config.yaml", "config file")
+}
+
+// GetConfig to struct, configs should be the point of config struct
+func GetConfig(configs interface{}) error {
+	viper.SetConfigFile(cfgFile)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	if err := viper.Unmarshal(configs); err != nil {
+		return err
+	}
+	return nil
+}
